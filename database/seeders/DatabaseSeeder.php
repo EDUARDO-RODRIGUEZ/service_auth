fa7cd0b3c064eac55212d32164e99b17ba4da721
Eduardo <jorge.ecrg@gmail.com>
2022-12-13 11:59:16 -0400

    Agregar Admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,6 +23,15 @@ class DatabaseSeeder extends Seeder
             [
                 "nombre" => "cliente",
                 "descripcion" => "cliente del sistema",
+            ]
+        ]);
+        DB::table("usuarios")->insert([
+            [
+                "nombre" => "eduardo",
+                "email" => "jorge.ecrg@gmail.com",
+                "password" => Hash::make("12345678"),
+                "perfil" => "admin.jpg",
+                "rol_id" => 1,
             ]
         ]);
     }
